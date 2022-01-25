b87ac35aeb878108df8ddc587bfe5a1d0792883d
Kamil Moszczyc <likeyouop@gmail.com>
2022-01-25 01:08:39 +0100

    Add portfolio project

---
+++ script.js
@@ -0,0 +1,34 @@
+let theme = localStorage.getItem('theme')
+if(theme==null) {
+    setTheme('light')
+} else {
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (let i=0; i< themeDots.length; i++) {
+    themeDots[i].addEventListener('click', function () {
+        let mode = this.dataset.mode;
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode) {
+    switch (mode){
+        case 'light':
+            document.getElementById('theme-style').href = 'default.css'
+            break;
+        case 'blue':
+            document.getElementById('theme-style').href = 'blue.css'
+            break;
+        case 'green':
+            document.getElementById('theme-style').href = 'green.css'
+            break
+        case 'purple':
+            document.getElementById('theme-style').href = 'purple.css'
+            break;
+    }
+
+    localStorage.setItem('theme', mode)
+}
